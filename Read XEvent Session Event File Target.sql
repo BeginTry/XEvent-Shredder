d7c513e75fd5cecbe210a4f04149db82c1c43dab
Dave Mason <DMasonSQL@GMail.com>
2020-11-04 12:27:55 -0500

    Update Read XEvent Session Event File Target.sql
    Adjust UTC time to local server time.
    Output as XML to overcome character/output limitations of PRINT.

--- Read XEvent Session Event File Target.sql
+++ Read XEvent Session Event File Target.sql
@@ -3,40 +3,40 @@ DECLARE @Tsql NVARCHAR(MAX) = '';
 
 ;WITH AllSessionEventFields AS
 (
- --Unique Global Fields (Actions) across all events for the session.
- SELECT DISTINCT sa.name EventField, 'action' AS XmlNodeName, 
-  CASE WHEN x.SqlDataType IS NULL THEN 'text' ELSE 'value' END AS XmlSubNodeName,
-  'Global Fields (Action)' AS FieldType, o.type_name XE_type, 
-  COALESCE(x.SqlDataType, 'NVARCHAR(MAX)') AS SqlDataType
- FROM sys.server_event_sessions s
- JOIN sys.server_event_session_events se
-	ON se.event_session_id = s.event_session_id
- JOIN sys.server_event_session_actions sa
-	ON sa.event_session_id = s.event_session_id
-	AND sa.event_id = se.event_id
- JOIN sys.dm_xe_objects o
-	ON o.name = sa.name
-	AND o.object_type = 'action'
- LEFT JOIN master.dbo.XE_TSQL_TypeXref x
-	ON x.XE_type = o.type_name
- WHERE s.name = @XESessionName
+	--Unique Global Fields (Actions) across all events for the session.
+	SELECT DISTINCT sa.name EventField, 'action' AS XmlNodeName, 
+		CASE WHEN x.SqlDataType IS NULL THEN 'text' ELSE 'value' END AS XmlSubNodeName,
+		'Global Fields (Action)' AS FieldType, o.type_name XE_type, 
+		COALESCE(x.SqlDataType, 'NVARCHAR(MAX)') AS SqlDataType
+	FROM sys.server_event_sessions s
+	JOIN sys.server_event_session_events se
+		ON se.event_session_id = s.event_session_id
+	JOIN sys.server_event_session_actions sa
+		ON sa.event_session_id = s.event_session_id
+		AND sa.event_id = se.event_id
+	JOIN sys.dm_xe_objects o
+		ON o.name = sa.name
+		AND o.object_type = 'action'
+	LEFT JOIN master.dbo.XE_TSQL_TypeXref x
+		ON x.XE_type = o.type_name
+	WHERE s.name = @XESessionName
 
- UNION
+	UNION
 
- --Unique Event Fields across all events for the session.
-SELECT DISTINCT c.name EventField, 'data' AS XmlNodeName, 
-	CASE WHEN x.SqlDataType IS NULL THEN 'text' ELSE 'value' END AS XmlSubNodeName,
-	'Event Fields' AS FieldType, c.type_name XE_type, 
-	COALESCE(x.SqlDataType, 'NVARCHAR(MAX)') AS SqlDataType
-FROM sys.server_event_sessions s
-JOIN sys.server_event_session_events se
-	ON se.event_session_id = s.event_session_id
-JOIN sys.dm_xe_object_columns c
-	ON c.object_name = se.name
-	AND c.column_type = 'data' 
-LEFT JOIN master.dbo.XE_TSQL_TypeXref x
-	ON x.XE_type = c.type_name
-WHERE s.name = @XESessionName
+	 --Unique Event Fields across all events for the session.
+	SELECT DISTINCT c.name EventField, 'data' AS XmlNodeName, 
+		CASE WHEN x.SqlDataType IS NULL THEN 'text' ELSE 'value' END AS XmlSubNodeName,
+		'Event Fields' AS FieldType, c.type_name XE_type, 
+		COALESCE(x.SqlDataType, 'NVARCHAR(MAX)') AS SqlDataType
+	FROM sys.server_event_sessions s
+	JOIN sys.server_event_session_events se
+		ON se.event_session_id = s.event_session_id
+	JOIN sys.dm_xe_object_columns c
+		ON c.object_name = se.name
+		AND c.column_type = 'data' 
+	LEFT JOIN master.dbo.XE_TSQL_TypeXref x
+		ON x.XE_type = c.type_name
+	WHERE s.name = @XESessionName
 )
 SELECT @Tsql = @Tsql + CHAR(9) + 
 	CASE
@@ -65,8 +65,10 @@ SELECT @Tsql = ';WITH XEvents AS
 	FROM sys.fn_xe_file_target_read_file ( ''' + @XESessionName + '*.xel'', NULL, NULL, NULL )  
 )
 SELECT object_name, event_data,' + CHAR(13) + CHAR(10) + '
-	event_data.value (''(/event/@timestamp)[1]'', ''DATETIME'') AS [timestamp],' + CHAR(13) + CHAR(10) + @Tsql + '
+	--XEvent timestamp is UTC. Adjust for local server time.
+	DATEADD(HOUR, DATEDIFF(HOUR, GETUTCDATE(), GETDATE()), event_data.value (''(/event/@timestamp)[1]'', ''DATETIME'')) AS [timestamp],' + CHAR(13) + CHAR(10) + @Tsql + '
 FROM XEvents;';
 
-PRINT @Tsql;
-EXEC(@Tsql);
+--PRINT @Tsql;
+SELECT [processing-instruction(x)] = @Tsql FOR XML PATH(''),TYPE
+--EXEC(@Tsql);
